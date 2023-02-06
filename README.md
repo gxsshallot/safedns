9ff2f2eb3b90c5d83e455164a208bf115778df1d
Xiaosong Gao <gxsshallot@163.com>
2023-02-06 13:55:51 +0800

    添加读取键盘输入的方式。

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 查询有效的IP地址工具，之后输出的内容，可以直接粘贴到`hosts`文件夹中使用。
 
 ```shell
-# 使用方法
-safedns [TXT_PATH]
+# 直接运行的方式(手动输入域名)
+safedns
 
+# 读取文件的方式
+safedns [TXT_PATH]
 # 例如：github.com
 safedns ./data/github.txt
 ```
--- cmd/main.go
+++ cmd/main.go
@@ -10,25 +10,50 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) > 2 {
 		fmt.Printf("使用格式: safedns [TXT_PATH]\n")
 		os.Exit(1)
 	}
-	txtPath := os.Args[1]
-	content, err := ioutil.ReadFile(txtPath)
-	if err != nil {
-		fmt.Printf("读取文件错误: %s", err)
-		os.Exit(1)
+	var domains []string
+	if len(os.Args) == 2 {
+		txtPath := os.Args[1]
+		content, err := ioutil.ReadFile(txtPath)
+		if err != nil {
+			fmt.Printf("读取文件错误: %s", err)
+			os.Exit(1)
+		}
+		domains = strings.Split(string(content), "\n")
+	} else {
+		var domain string
+		for {
+			fmt.Printf("请输入一个域名: ")
+			_, err := fmt.Scanln(&domain)
+			if err != nil {
+				continue
+			}
+			if len(domain) == 0 {
+				continue
+			}
+			break
+		}
+		domains = []string{domain}
 	}
-	lines := strings.Split(string(content), "\n")
+
 	result := []string{}
-	for _, domain := range lines {
+	for _, domain := range domains {
 		ipAddr := safedns.ValidIp(domain)
 		if len(ipAddr) > 0 {
 			result = append(result, fmt.Sprintf("%s %s", ipAddr, domain))
 		}
 	}
 	fmt.Println()
-	fmt.Printf("以下为hosts内容:\n")
-	fmt.Printf("%s\n", strings.Join(result, "\n"))
+	if len(result) > 0 {
+		fmt.Printf("以下为hosts内容:\n")
+		fmt.Printf("%s\n", strings.Join(result, "\n"))
+	} else {
+		fmt.Println("未找到合适的IP地址")
+	}
+	fmt.Println()
+	fmt.Println("按回车键退出")
+	fmt.Scanln()
 }
